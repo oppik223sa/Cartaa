ea77816da6e5d34fa5ae6668dc7482aaec0058a9
oppik223sa <80780103+oppik223sa@users.noreply.github.com>
2026-02-09 01:19:49 -0400

    Remove width settings for curtains on page load
    Remove initial width settings for left and right curtains.

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     // Al cargar la página, ocultamos las cortinas
-    $('.left-curtain').css('width', '0%');
-    $('.right-curtain').css('width', '0%');
 
     $('.valentines-day').click(function () {
         // Animación de desvanecimiento de los elementos del sobre
